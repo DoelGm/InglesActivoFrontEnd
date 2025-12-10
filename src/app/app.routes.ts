012b0d3ce6f2107ab3954d8e41d219bc7e69c0d3
Kallolt2030 <ktbernalgallegos@gmail.com>
2025-12-09 18:57:06 -0600

    add students and studets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { PublicComponent } from './core/layout/pages/public/public.component';
 import { HomePageComponent } from './feactures/home/pages/home-page/home-page.component';
 import { LoginPageComponent } from './feactures/login/pages/login-page/login-page.component';
 import { GroupPageComponent } from './feactures/groups/pages/group-page/group-page.component';
+import { StudentsComponent } from './feactures/admin/components/students/students.component';
+import { AddStudentsComponent } from './feactures/admin/components/add-students/add-students.component';
 
 export const routes: Routes = [
     {
@@ -32,6 +34,22 @@ export const routes: Routes = [
             { path: '', component: GroupPageComponent } 
         ]
         },
+
+       {
+        path: 'students',
+        component: PublicComponent,
+        children: [
+            { path: '', component: StudentsComponent } 
+        ]
+        },
+               {
+        path: 'addStudents',
+        component: PublicComponent,
+        children: [
+            { path: '', component: AddStudentsComponent } 
+        ]
+        },
+
     // {
     //     path: 'admin',
     //     component: AdminComponent,
---
+++ src/app/feactures/admin/components/add-students/add-students.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-students',
+  imports: [],
+  templateUrl: './add-students.component.html',
+  styleUrl: './add-students.component.css'
+})
+export class AddStudentsComponent {
+
+}
